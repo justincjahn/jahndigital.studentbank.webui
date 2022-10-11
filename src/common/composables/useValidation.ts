c0022112ae34cf3898857f2be6b8b6ba5ce44099
Justin Jahn <justin@justinjahn.me>
2022-10-10 22:43:41 -0700

    Made useValidation generic.

--- src/common/composables/useValidation.ts
+++ src/common/composables/useValidation.ts
@@ -3,21 +3,21 @@ import { ref, Ref, unref, watchEffect } from 'vue';
 /**
  * Callback function passed to validator utilities to check input values.
  */
-export type ValidationFunc = (
-  value: string
+export type ValidationFunc<T = string> = (
+  value: T | null
 ) => string | boolean | Promise<string | boolean>;
 
 /**
  * Function called to format a value before it's validated.
  */
-export type DecoratorFunc = (value: string) => string;
+export type DecoratorFunc<T = string> = (value: T | null) => T | null;
 
 /**
  * Options object passed to the useValidation function.
  */
-export interface UseValidationOptions {
-  decorator?: DecoratorFunc | Ref<DecoratorFunc>;
-  value?: Ref<string>;
+export interface UseValidationOptions<T = string> {
+  decorator?: DecoratorFunc<T> | Ref<DecoratorFunc<T>>;
+  value?: Ref<T | null>;
   error?: Ref<string>;
   immediate?: boolean;
 }
@@ -28,21 +28,21 @@ export interface UseValidationOptions {
  * @param validator
  * @param options
  */
-export default function useValidation(
-  validator: ValidationFunc | Ref<ValidationFunc>,
-  options?: UseValidationOptions
+export default function useValidation<T = string>(
+  validator: ValidationFunc<T> | Ref<ValidationFunc<T>>,
+  options?: UseValidationOptions<T>
 ): {
-  value: Ref<string>;
+  value: Ref<T | null>;
   error: Ref<string>;
   loading: Ref<boolean>;
 } {
   const opts = options || {};
-  const value = opts?.value ?? ref('');
+  const value = opts?.value ?? (ref(null) as Ref<T | null>);
   const error = opts?.error ?? ref('');
   const loading = ref(false);
   let initialValidation = true;
 
-  async function runValidation(newValue: string) {
+  async function runValidation(newValue: T | null) {
     if (initialValidation) {
       initialValidation = false;
 
