5c8f912903264a1fdf2a9a07d0b01fef721266f5
Justin Jahn <justin@justinjahn.me>
2023-07-31 01:51:10 -0700

    Added optional params to student route.

--- src/admin/students/routes.ts
+++ src/admin/students/routes.ts
@@ -8,7 +8,7 @@ const routes: RouteRecordRaw[] = [
 
     children: [
       {
-        path: '',
+        path: ':studentId?',
         name: RouteNames.index,
         component: () => import('./pages/StudentsIndex.vue'),
       },
