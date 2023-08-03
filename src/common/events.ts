f3001eb08668c9fc7e4009806ab4bcea2c43bc54
Justin Jahn <justin@justinjahn.me>
2023-08-02 21:51:33 -0700

    New event: shareBalanceChange

--- src/common/events.ts
+++ src/common/events.ts
@@ -1,5 +1,6 @@
 import type { UserInfo, StudentInfo } from '@/common/stores/user';
 import { create } from '@/common/services/eventBus';
+import type { IMoney } from './utils/Money';
 
 // Fired when a user logs in to the application successfully
 export const userLogin = create<UserInfo | StudentInfo>('user-login');
@@ -9,3 +10,11 @@ export const userLogout = create('user-logout');
 
 // Fired when a new error is logged in the errorStore
 export const newError = create<string | null>('error-new');
+
+// Fired when a share's balance changes
+export interface BalanceChange {
+  ShareId: number;
+  NewBalance: IMoney;
+}
+
+export const shareBalanceChange = create<BalanceChange>('share-balance-change');
