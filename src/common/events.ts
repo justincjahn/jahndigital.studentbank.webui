c088afa37abb46e82168b75e07ba9f0e11b268bd
Justin Jahn <justin@justinjahn.me>
2023-08-03 22:38:12 -0700

    Tweaked newTransaction event.

--- src/common/events.ts
+++ src/common/events.ts
@@ -1,6 +1,6 @@
 import type { UserInfo, StudentInfo } from '@/common/stores/user';
+import type { Transaction } from '@/common/services/transaction';
 import { create } from '@/common/services/eventBus';
-import type { IMoney } from './utils/Money';
 
 // Fired when a user logs in to the application successfully
 export const userLogin = create<UserInfo | StudentInfo>('user-login');
@@ -11,10 +11,5 @@ export const userLogout = create('user-logout');
 // Fired when a new error is logged in the errorStore
 export const newError = create<string | null>('error-new');
 
-// Fired when a share's balance changes
-export interface BalanceChange {
-  ShareId: number;
-  NewBalance: IMoney;
-}
-
-export const shareBalanceChange = create<BalanceChange>('share-balance-change');
+// Fired when a new transaction is posted
+export const newTransaction = create<Transaction>('transaction-new');
