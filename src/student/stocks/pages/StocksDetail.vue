904932be2a847444fbe380c3333c35cd0a041861
Justin Jahn <justin@justinjahn.me>
2023-09-21 21:52:08 -0700

    ApexCharts for stock detail.

--- src/student/stocks/pages/StocksDetail.vue
+++ src/student/stocks/pages/StocksDetail.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import type { ApexOptions } from 'apexcharts';
 import type { Stock } from '@/common/services/stock';
 
 // Core
-import { ref, toRef, computed } from 'vue';
+import { defineAsyncComponent, ref, toRef, computed } from 'vue';
 import { useRoute } from 'vue-router';
 
 // Stores
@@ -14,6 +15,9 @@ import useGlobalStore from '@/student/common/composables/useGlobalStore';
 // Components
 import LoadingPage from '@/common/pages/LoadingPage.vue';
 import StockHistoryList from '@/student/stocks/components/StockHistoryList.vue';
+import Money from '@/common/utils/Money';
+
+const apexchart = defineAsyncComponent(() => import('vue3-apexcharts'));
 
 const router = useRoute();
 
@@ -38,6 +42,56 @@ const description = computed(() =>
     ? stock.value.formattedDescription
     : 'No description available.'
 );
+
+const chartOptions = computed(() => {
+  const values = stockHistoryStore.history.value;
+  const latest = values[0]?.value ?? -1;
+  const previous = values[1]?.value ?? -1;
+  const color = latest >= previous ? '#198754' : '#DC3848';
+
+  return {
+    colors: [color],
+    xaxis: {
+      type: 'datetime',
+    },
+    yaxis: {
+      labels: {
+        formatter(val) {
+          return Money.fromNumber(val).toString();
+        },
+      },
+    },
+    tooltip: {
+      y: {
+        formatter(val) {
+          return Money.fromNumber(val).toString();
+        },
+      },
+      x: {
+        format: 'dd MMM yyyy',
+      },
+    },
+    chart: {
+      zoom: {
+        autoScaleYaxis: true,
+      },
+    },
+  } as ApexOptions;
+});
+
+const chartSeries = computed(() => {
+  const data = stockHistoryStore.history.value.map((history) => ({
+    x: new Date(history.dateChanged).getTime(),
+    y: history.value,
+  }));
+
+  return [
+    {
+      name: 'Value',
+      data,
+    },
+  ] as ApexAxisChartSeries;
+});
 
 async function fetchById() {
   if (stockId.value > 0) {
@@ -73,10 +127,24 @@ fetchById();
 <template>
   <!-- eslint-disable vue/no-v-html -->
   <div class="container section flow" :class="{ loading: stockLoading }">
-    <LoadingPage v-if="stockLoading" />
+    <loading-page v-if="stockLoading" />
     <template v-else>
       <h2 class="size-xl">{{ stock?.name ?? 'Loading...' }}</h2>
       <div v-html="description" />
+
+      <suspense>
+        <apexchart
+          :options="chartOptions"
+          :series="chartSeries"
+          type="line"
+          width="100%"
+          height="250rem"
+        />
+
+        <template #fallback>
+          <loading-page />
+        </template>
+      </suspense>
 
       <div :class="{ loading: historyLoading }">
         <stock-history-list :store="stockHistoryStore" />
